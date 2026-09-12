969435e3caf06cbe7193a4da35f711730564f23c
daki <nezoko45@gmail.com>
2026-09-12 07:24:28 -0500

    Expose VoiceChanger driver controls to desktop UI

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -3,4 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('nativeAudio', {
   playWavBase64: (base64) => ipcRenderer.invoke('native-audio:play-wav', base64),
   stop: () => ipcRenderer.invoke('native-audio:stop'),
+  driverStatus: () => ipcRenderer.invoke('voicechanger-driver:status'),
+  driverInstall: () => ipcRenderer.invoke('voicechanger-driver:install'),
+  driverUninstall: () => ipcRenderer.invoke('voicechanger-driver:uninstall'),
 });
