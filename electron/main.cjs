5fadfe573c9ee52cfbf31bb78770680469d1fe1d
daki <nezoko45@gmail.com>
2026-09-22 13:09:31 -0500

    Rebuild Electron as the main UI and audio app

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,9 +1,24 @@
 const {app,BrowserWindow,session}=require("electron");
-const path=require("node:path");
+
 let win=null;
+
 function createWindow(){
- win=new BrowserWindow({show:false,width:420,height:220,backgroundColor:"#111111",webPreferences:{contextIsolation:true,sandbox:true,nodeIntegration:false}});
- win.setMenuBarVisibility(false);win.loadFile(path.join(__dirname,"source.html"));win.on("closed",()=>{win=null});
+  win=new BrowserWindow({
+    width:900,
+    height:720,
+    minWidth:760,
+    minHeight:620,
+    backgroundColor:"#0d1117",
+    show:false,
+    webPreferences:{contextIsolation:true,sandbox:true,nodeIntegration:false}
+  });
+  win.setMenuBarVisibility(false);
+  session.defaultSession.setPermissionRequestHandler((_wc,permission,callback)=>{
+    callback(permission==="media");
+  });
+  win.loadURL("http://127.0.0.1:8765/");
+  win.once("ready-to-show",()=>win.show());
+  win.on("closed",()=>{win=null});
 }
-app.whenReady().then(()=>{session.defaultSession.setPermissionRequestHandler((_wc,permission,callback)=>callback(permission==="media"));createWindow()});
+app.whenReady().then(createWindow);
 app.on("window-all-closed",()=>{});
